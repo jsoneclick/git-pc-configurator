74fd1fb7635c07081aa527e11466aa448ec5e6b7
jsoneclick <51366467+jsoneclick@users.noreply.github.com>
2023-07-31 02:37:46 +0300

    Changes
    1. Preloader
    2. Fixed animations
    3. Changed text
    4.Fixed gallery
    5. changed images

--- src/script/app.js
+++ src/script/app.js
@@ -3,6 +3,7 @@ const mainBlock = document.querySelector(".mainContainer");
 const infoBlock = document.querySelector(".infoWrapper");
 const preloaderVideo = document.querySelector(".preloaderVideo");
 const constructorWrapper = document.querySelector(".constructorWrapper");
+const preloader = document.getElementById("universe");
 
 function changeToConf() {
     setTimeout(function(){
@@ -12,12 +13,15 @@ function changeToConf() {
         setTimeout(function(){
             infoBlock.style.display = "none";
             preloaderVideo.style.display = "block";
+            preloader.style.display = "block";
             setTimeout(function(){
                 preloaderVideo.style.opacity = ".3";
                 setTimeout(function(){
                     preloaderVideo.style.opacity = 0;
+                    preloader.style.display = "none";
                     setTimeout(function(){
                         preloaderVideo.style.display = "none";
+                        
                         setTimeout(function(){
                             constructorWrapper.style.display = "block";
                         }, 500);
